5bc354423f7e6f96df8e5f8891d10d4c732ee0ed
KarthikC-777 <karthikc522001@gmail.com>
2024-05-07 11:04:07 +0530

    feature: users apis for admin created.

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import mongoose from 'mongoose';
 import { Roles } from '../../decorators/role.decorator';
 import { ExtractKeyFromRequest } from '../../decorators/user-decorator';
@@ -6,12 +6,13 @@ import { SuccessResponseType, UserRequestType } from '../@types';
 import { UserRole } from './enums/user-designation.enum';
 import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
+import { constructErrorMessage } from '../../utils/response-message.util';
 
-@Controller('user')
+@Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get('/')
+  @Get('/user')
   @Roles(UserRole.Admin, UserRole.Employee)
   async fetchUserDetails(
     @ExtractKeyFromRequest('user') user: UserRequestType,
@@ -22,6 +23,41 @@ export class UserController {
     return {
       statusCode: 200,
       message: `User info fetched successfully`,
+      result: userData,
+    };
+  }
+
+  @Get('/users')
+  @Roles(UserRole.Admin, UserRole.Employee)
+  async fetchAllUserDetails(): Promise<SuccessResponseType<User[]>> {
+    const usersData = await this.userService.getAllUserDetails();
+    return {
+      statusCode: 200,
+      message: `Users list fetched successfully`,
+      result: usersData,
+    };
+  }
+
+  @Get('/users/:userId')
+  @Roles(UserRole.Admin, UserRole.Employee)
+  async fetchUserDetailsById(
+    @Param('userId') userId: string,
+  ): Promise<SuccessResponseType<User>> {
+    const userData = await this.userService.getUserDetails({
+      _id: new mongoose.Types.ObjectId(userId),
+    });
+    if (!userData) {
+      throw new NotFoundException(
+        constructErrorMessage(
+          'DB.MODULE_NOT_FOUND',
+          { MODULE: 'User' },
+          'User.NOT_FOUND',
+        ),
+      );
+    }
+    return {
+      statusCode: 200,
+      message: `User info fetched successfully for userId- ${userId}`,
       result: userData,
     };
   }
